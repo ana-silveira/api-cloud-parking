2c920f28e4deabaebb005fee02cc46e44f7894ef
Ana Carolina <ana.fbsilveira@gmail.com>
2022-10-05 14:34:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# api-cloud-parking
+ 
---
+++ src/main/java/com/parkingapi/cloudparking/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.parkingapi.cloudparking.controller;
+
+/*
+Classe de retorno de String "Hello".
+Quando eu for no browser e digitar o localhost:8080,
+a mensagem será disparada nesse endpoint.
+*/
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/")
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "HELLO . Java Devs";
+    }
+}
